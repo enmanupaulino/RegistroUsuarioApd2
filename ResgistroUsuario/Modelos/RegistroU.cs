868d25a381a6fb12a10c5d135547d6ed426784a3
Emmanuel <Emmanuel@DESKTOP-B6P5QMH>
2020-01-15 19:03:35 -0400

    avanzando

--- ResgistroUsuario/Modelos/RegistroU.cs
+++ ResgistroUsuario/Modelos/RegistroU.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,15 @@ namespace ResgistroUsuario.Modelos
     {
         public int ID { get; set; }
         public string Nombre { get; set; }
+
         public string Apellidos { get; set; }
+        
+
         public string Usuario { get; set; }
+      
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
 
 
